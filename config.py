93fb2efff9b1bf39d81c9023434d66b2630c4b64
Tushar  Gaur <tushargaur3312@gmail.com>
2024-06-14 04:33:09 +0530

    deleted:    config.py 	modified:   procfile 	modified:   run.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    bind = "0.0.0.0:8080"
-    workers = 2
--- run.py
+++ run.py
@@ -8,4 +8,4 @@ def make_shell_context():
     return {'db': db, 'User': User}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=8080)
