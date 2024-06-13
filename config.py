a24cedbe6bc9caac56bb66eddbfec452c7eaf225
Tushar  Gaur <tushargaur3312@gmail.com>
2024-06-14 04:29:02 +0530

    modified:   procfile

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
-bind = "0.0.0.0:8080"
-workers = 2
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    bind = "0.0.0.0:8080"
+    workers = 2
